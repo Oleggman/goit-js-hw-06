8168898b3c35f05ae67b142716060e34966183b5
Олег <olegkovalets1112003@knu.ua>
2023-07-28 16:13:54 +0300

    Finish task 7

---
+++ js/task-07.js
@@ -0,0 +1,13 @@
+const refs = {
+  input: document.getElementById("font-size-control"),
+  text: document.getElementById("text"),
+};
+
+refs.input.value = 16;
+refs.input.addEventListener("input", onRangeInput);
+
+function onRangeInput(event) {
+  //   refs.text.style.display = "inline-block";
+
+  refs.text.style.fontSize = `${event.currentTarget.value}px`;
+}
