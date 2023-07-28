486fa04f58e6c1ced0c81eff6709507a729224b6
Олег <olegkovalets1112003@knu.ua>
2023-07-28 17:23:15 +0300

    Finish task 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  colorSpan: document.querySelector(".color"),
+  colorBtn: document.querySelector(".change-color"),
+};
+
+refs.colorBtn.addEventListener("click", onChangeColor);
+
+function onChangeColor() {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  refs.colorSpan.textContent = color;
+}
