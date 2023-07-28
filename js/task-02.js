43a2c53b316b105712780dea0b4471f4afb8bc8d
Олег <olegkovalets1112003@knu.ua>
2023-07-28 15:32:05 +0300

    Finish task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,11 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
+
+const ingredientsRef = document.querySelector("#ingredients");
+const items = ingredients.map((item) => {
+  const li = document.createElement("li");
+  li.textContent = item;
+  li.classList.add("item");
+  return li;
+});
+
+ingredientsRef.append(...items);
