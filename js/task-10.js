6ea37b160b693f62a35e231fba480f8ee8df95d1
Олег <olegkovalets1112003@knu.ua>
2023-07-29 10:23:56 +0300

    Finish task 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,35 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const controlsRef = document.getElementById("controls");
+
+const refs = {
+  numberInput: controlsRef.firstElementChild,
+  createBtn: controlsRef.querySelector("[data-create]"),
+  destroyBtn: controlsRef.querySelector("[data-destroy]"),
+  boxesRef: document.querySelector("#boxes"),
+};
+
+let boxSize = 30;
+let amount = 0;
+
+function createBoxes(amount) {
+  let markup = "";
+  for (let i = 0; i < amount; i++) {
+    markup += `<div style="width: ${boxSize}px; height: ${boxSize}px; background-color: ${getRandomHexColor()}"></div>`;
+    boxSize += 10;
+  }
+
+  destroyBoxes();
+  refs.boxesRef.insertAdjacentHTML("beforeend", markup);
+}
+
+function destroyBoxes() {
+  refs.boxesRef.innerHTML = "";
+  boxSize = 30;
+}
+
+refs.numberInput.addEventListener("change", (event) => (amount = Number(event.currentTarget.value)));
+refs.createBtn.addEventListener("click", () => createBoxes(amount));
+refs.destroyBtn.addEventListener("click", () => destroyBoxes());
